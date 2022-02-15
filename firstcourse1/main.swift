b3ff16a0d83620cec030b4ba8cb15e4c7a856a10
AntropovSV <pensatum@gmail.com>
2022-02-15 22:15:37 +0300

    Home work 3

--- firstcourse1/main.swift
+++ firstcourse1/main.swift
@@ -7,5 +7,85 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Car {
+
+    let type: String
+    var year: Int
+    let volume: Int
+    var enganeWork: Bool
+    var windowOpen: Bool
+    var emptyVolume: Bool
+    let mark: String
+    let colour: String
+    var mileage: Int
+
+}
+
+var carOne = Car.init(
+    type: "sport car",
+    year: 2021,
+    volume: 1000,
+    enganeWork: false,
+    windowOpen: true,
+    emptyVolume: false,
+    mark: "Chevrolet",
+    colour: "yellow",
+    mileage: 5000)
+var carTwo = Car.init(
+    type: "trunkCar",
+    year: 2015, volume: 2000,
+    enganeWork: false,
+    windowOpen: false,
+    emptyVolume: true,
+    mark: "Audi",
+    colour: "green",
+    mileage: 41000)
+
+print(" характеристики авто  \(carOne) ")
+print(" характеристики авто  \(carTwo) ")
+
+
+
+enum Condition {
+    case on
+    case off
+}
+
+let on = Condition.on
+let off = Condition.off
+
+func chooseCondition (engane: Condition, window: Condition, volume: Condition) {
+    switch engane {
+    case .on:
+        carOne.enganeWork = true
+        carTwo.enganeWork = true
+    case .off:
+        carOne.enganeWork = false
+        carTwo.enganeWork = false
+       
+    }
+    
+    switch window {
+    case .on:
+        carOne.windowOpen = true
+        carTwo.windowOpen = true
+    case .off:
+        carOne.windowOpen = false
+        carTwo.windowOpen = false
+    }
+    
+    switch volume {
+    case .on:
+        carOne.emptyVolume = true
+        carTwo.emptyVolume = true
+    case .off:
+        carOne.emptyVolume = false
+        carTwo.emptyVolume = false
+    }
+    
+}
+chooseCondition(engane: on, window: on, volume: off)
+
+print(carOne)
+print(carTwo)
 
